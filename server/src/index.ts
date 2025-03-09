627f25cb97503ad7524c8de8fb69ef46cbbd32d1
have2b <thanhlongvu156@gmail.com>
2025-03-10 02:30:26 +0700

    feat: temp cors for dev and set base path for api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const app = new Hono();
+const app = new Hono().basePath("/api");
+
+app.use("*", cors());
 
 app.get("/", (c) => {
   return c.text("Hello World!");
