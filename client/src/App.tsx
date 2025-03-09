4082b5adfed7c6acf4432a68344340b20948f851
have2b <thanhlongvu156@gmail.com>
2025-03-10 03:01:52 +0700

    fix: connect azure via oidc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ function App() {
   return (
     <section className="flex-row justify-center items-center w-full">
       <h1>Hello World</h1>
-      <p>Deployment successfully</p>
     </section>
   );
 }
