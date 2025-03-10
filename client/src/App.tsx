574ec90d522ac1fafbedb20da7cdd5291d37ed73
have2b <thanhlongvu156@gmail.com>
2025-03-11 00:03:03 +0700

    try to change ingress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <section className="flex-row justify-center items-center w-full">
       <h1>Hello World</h1>
-      <p>After config CICD</p>
+      <p>Change the ingress</p>
     </section>
   );
 }
