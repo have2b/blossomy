de86b8bd424bf81a5679d8d8046c1f9800f24cdb
have2b <thanhlongvu156@gmail.com>
2025-03-11 00:23:22 +0700

    try add vite url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,21 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const callApi = async () => {
+      const response = await axios.get(
+        import.meta.env.VITE_API_URL + "/api/me"
+      );
+      console.log(response);
+    };
+
+    callApi();
+  }, []);
+
   return (
     <section className="flex-row justify-center items-center w-full">
       <h1>Hello World</h1>
-      <p>Change the ingress</p>
     </section>
   );
 }
